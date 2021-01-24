149ba19d1b2cf5a272180f00586bd5f08fd940d5
albertlockett <albert.lockett@gmail.com>
2021-01-24 12:05:10 -0400

    STAT-3093 assignment 2

---
+++ STAT_3093/r/assignment_2/question_1.js
@@ -0,0 +1,34 @@
+
+const mean = sample => 
+  sample.reduce((a, b) => a+b, 0) / sample.length
+
+const stdev = (sample, sm=mean(sample)) =>
+  Math.sqrt(sample.reduce((a, b) => a + Math.pow(b - sm, 2), 0))
+
+const means = {}
+const stdevs = {}
+let observations = []
+
+for (let i of [0, 1, 2]) {
+  for (let j of [0, 1, 2]) {
+    let sample = [i, j]
+    sampleMean = mean(sample)
+    sampleStdev = stdev(sample)
+    
+    means[sampleMean] = 1 + (means[sampleMean] ?? 0)
+    stdevs[sampleStdev] = 1 + (stdevs[sampleStdev] ?? 0)
+    observations = [...observations, {i, j, sampleMean, sampleStdev }]
+  }
+}
+
+const combinations = observations.length
+
+console.table(observations)
+
+console.table(
+  Object.keys(means).sort()
+    .reduce((acc, x) => [ ...acc, { x, probabilty: `${means[x]}/${combinations}` }], []))
+
+console.table(
+  Object.keys(stdevs).sort()
+    .reduce((acc, x) => [ ...acc, { x, probabilty: `${stdevs[x]}/${combinations}` }], []))
